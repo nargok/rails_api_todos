8ae74379b7a7d3a741919953fa800808c842a923
nargok <nargok@gmail.com>
2019-05-14 21:22:08 +0900

    model test for todo & item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belongs_to(:todo) }
+
+  it { should validate_presense_of(:name) }
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure Todo model has a 1:m relationship with the Item model
+  it { should have_may(:items).dependent(:destory) }
+
+  # Validation test
+  # ensure columns title and created_by are present
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:created_by) }
 end
